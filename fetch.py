abd9833cfa999999d327a0ae91e8fa0079a10c6c
Yubo <you-2001-3-6@ezweb.ne.jp>
2020-12-09 14:57:40 +0900

    first

---
+++ fetch.py
@@ -0,0 +1,59 @@
+import json
+import sys
+from typing import Any, AnyStr, Tuple, List, Union, Dict, Iterable, Optional, Callable
+import requests
+
+def fetch(url: AnyStr, params: Dict={}) -> Optional[requests.models.Response]:
+    res: requests.models.Response = requests.get(url, params=params)
+    if res.status_code == 200:
+        return res
+    else:
+        return None
+
+def res_to_string(input_: requests.models.Response) -> Dict:
+    output: Dict = input_.text
+    
+    return output
+
+def decode(input_: AnyStr) -> Dict:
+    output: Dict = json.loads(input_)
+
+    return output
+
+def encode(input_: Dict, indent: int=4) -> AnyStr:
+    output: AnyStr = json.dumps(input_, ensure_ascii=True, indent=indent)
+
+    return output
+
+def pipe_parser(input_: Any, *args: Optional[Callable]) -> Any:
+    funcs: Iterable = args
+    output: Any = None
+    for func in funcs:
+        try:
+            if output is not None:
+                output = func(output)
+            else:
+                output = func(input_)
+        except:
+            sys.stderr(f'{func.__repr__} does not support the pipe.')
+    
+    return output
+
+def tinify_response(input_: requests.models.Response, types: Union[dict, str]) -> Union[AnyStr, Dict]:
+    if types == dict:
+        funcs: List[Callable] = [res_to_string, decode]
+    elif types == str:
+        funcs: List[Callable] = [res_to_string, decode, encode]
+    else:
+        raise ValueError('types; dict or str')
+    output: Union[AnyStr, Dict] = pipe_parser(input_, res_to_string, decode, encode)
+
+    return output
+
+if __name__ == "__main__":
+    params: Dict = {'pair': 'btc_jpy', 'limit': 100}
+    res = fetch('https://coincheck.com/api/trades', params=params)
+    print(res)
+    data = tinify_response(res, str)
+
+    print(data)
